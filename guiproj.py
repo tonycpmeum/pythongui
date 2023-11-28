6a12a65c72237d4c6c46c1391fb9f82cdb5a4c56
Tony?? <tonycpmeum@gmail.com>
2023-11-28 22:50:05 +0800

    menu command new file

--- guiproj.py
+++ guiproj.py
@@ -1,4 +1,6 @@
 import os, json
+import calendar
+import datetime
 from pathlib import Path
 import tkinter as tk
 from tkinter import messagebox
@@ -9,6 +11,8 @@ data_path = Path("./data/")
 if not os.path.exists(data_path):
    os.makedirs(data_path)
 
+months_dict: dict = {month: index for index, month in enumerate(calendar.month_name) if month}
+
 class MyGUI:
    def __init__(self):
       self.root = tk.Tk(className="Poo")
@@ -18,10 +22,10 @@ class MyGUI:
       self.menu_bar = Menu(self.root)
       self.menu_files = Menu(self.menu_bar, tearoff=0)
       self.menu_bar.add_cascade(label="Files", menu=self.menu_files)
-      self.menu_bar.add_command(label="Save")
-
+      self.menu_bar.add_command(label="Save", state="disabled", command= lambda: [self.saveFile(), self.updateSubmenu(submenu_open)])
+   
       # Files Menu
-      self.menu_files.add_command(label="New")
+      self.menu_files.add_command(label="New", command=self.newFile)
 
       submenu_open = tk.Menu(self.menu_files, tearoff=0)
       self.updateSubmenu(submenu_open)
@@ -29,15 +33,13 @@ class MyGUI:
 
       self.root.config(menu=self.menu_bar)
 
-      # tabs
-      self.create_tab_control()
-      self.create_data_entry_content()
-      self.create_data_analysis_content()
+      self.tab_control = ttk.Notebook(self.root, padding="12")
 
-      self.tab_control.pack(expand=1, fill="both")
+      self.file_name: str = ""
+      self.data_json: dict = {}
+
       #spinbox
       # combobox
-
 
       self.root.mainloop()
 
@@ -49,33 +51,99 @@ class MyGUI:
             file_stem = file_path.stem
             submenu_open.add_command(label=file_stem, command=lambda path=file_path: self.loadFile(path))
 
+   def newFile(self):
+      new_file_window = tk.Toplevel(self.root)
+      new_file_window.geometry("360x200")
+      new_file_window.title("New File")
+      new_file_window.config(padx=20, pady=10)
+      new_file_window.grid_columnconfigure(0, weight=1)
+      new_file_window.grid_columnconfigure(1, weight=1)
+
+      fn_label = tk.Label(new_file_window, text="File name: ")
+      fn_label.grid(row=0, column=0, sticky='w', pady=12)
+
+      fn_entry = tk.Entry(new_file_window)
+      fn_entry.grid(row=0, column=1, sticky='we')
+
+      month_label = tk.Label(new_file_window, text="Starting Month: ")
+      month_label.grid(row=1, column=0, sticky='w', pady=12)
+
+      n = tk.StringVar()
+      month_selection = ttk.Combobox(new_file_window, textvariable=n, state="readonly", values=tuple(months_dict.keys()))
+      month_selection.current(datetime.datetime.now().month - 1)
+      month_value: int = datetime.datetime.now().month
+      month_selection.grid(row=1, column=1, sticky='we')
+
+      year_label = tk.Label(new_file_window, text="Starting Year: ")
+      year_label.grid(row=2, column=0, sticky='w', pady=12)
+
+      m = tk.StringVar()
+      curr_year = datetime.datetime.now().year
+      year_value: int = curr_year
+      year_selection = ttk.Combobox(new_file_window, textvariable=m, state="readonly", values=tuple(i for i in range(2020, curr_year + 1)))
+      year_selection.current(curr_year - 2020)
+      year_selection.grid(row=2, column=1, sticky='we')
+
+      def on_month_change(*args):
+         nonlocal month_value
+         n.set(month_selection.get())
+         month_value = months_dict[n.get()]
+      month_selection.bind("<<ComboboxSelected>>", on_month_change)
+      
+      def on_year_change(*args):
+         nonlocal year_value
+         m.set(year_selection.get())
+         year_value = int(m.get())
+      year_selection.bind("<<ComboboxSelected>>", on_year_change)
+
+      def valid_filename(file_name: str):
+         allowed_chars = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-")
+         if file_name == "": return False
+         return all(char in allowed_chars for char in file_name)
+
+      def on_submit():
+         nonlocal year_value, month_value
+         if not valid_filename(fn_entry.get()):
+            messagebox.showerror("New File Error!", "File name must not contain space or special characters.")
+         else:
+            self.file_name = fn_entry.get()
+            self.root.title(f"Record - {self.file_name}")
+            self.create_tab_control()
+            self.data_json = { year_value: { month_value: { } } }
+            self.menu_bar.entryconfig("Save", state="normal")
+            new_file_window.destroy()
+
+      submit_button = tk.Button(new_file_window, text="Create", command=on_submit)
+      submit_button.grid(row=3, column=0, columnspan=2, pady=12, sticky='we')
+
    def loadFile(self, file_path: Path):
       with open(file_path, 'r') as read_file:
-         text = json.load(read_file)
-         file_stem = file_path.stem
+         self.data_json = json.load(read_file)
 
-         self.root.title(f"Something - {file_stem}")
+      print(self.data_json)
+      file_stem = file_path.stem
+      self.root.title(f"Record - {file_stem}")
+      self.create_tab_control()
 
    def saveFile(self):
-      input_msg = self.input_content.get()
-      file_name = self.input_filename.get()
-
-      if file_name == "" or input_msg == "":
-         messagebox.showerror("You Noob.", "File Name / input is empty")
-      else:
-         file_path = data_path / (file_name + '.json')
-         with open(file_path, 'w') as write_file:
-            json.dump(input_msg, write_file)
-
-      self.root.title(f"Something - {file_name}")
+      file_path = data_path / (self.file_name + '.json')
+      with open(file_path, 'w') as write_file:
+         json.dump(self.data_json, write_file)
    
    def create_tab_control(self):
-      self.tab_control = ttk.Notebook(self.root, padding="12") 
+      if self.tab_control.winfo_ismapped(): 
+         print("Exist")
+         return
+
       self.data_entry_tab = ttk.Frame(self.tab_control)
       self.data_analysis_tab = ttk.Frame(self.tab_control)
       
       self.tab_control.add(self.data_entry_tab, text="Data Entry")
       self.tab_control.add(self.data_analysis_tab, text="Data Analysis")
+      self.tab_control.pack(expand=1, fill="both")
+      
+      self.create_data_entry_content()
+      self.create_data_analysis_content()
       
    def create_data_entry_content(self):
       label_one = tk.Label(self.data_entry_tab, text="Content for Data Entry")
